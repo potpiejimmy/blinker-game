c93676a22e295aa75bf3e637f420cd90189c3553
liese <liese@d7406fb1-6878-4198-8eeb-e43e52bed576>
2014-02-15 10:40:52 +0000

    added blinker gamer

---
+++ client/trunk/app/blinker-game-desktop/src/com/doogetha/blinkergame/Main.java
@@ -0,0 +1,16 @@
+package com.doogetha.blinkergame;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "blinker-game";
+		cfg.useGL20 = false;
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new BlinkerGame(), cfg);
+	}
+}
