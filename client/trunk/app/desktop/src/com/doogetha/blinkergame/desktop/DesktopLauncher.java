61e6ebb86588c6a4970e7335d44ceb97f7e0faef
liese <liese@d7406fb1-6878-4198-8eeb-e43e52bed576>
2014-04-17 20:57:59 +0000

    import initial grade project

---
+++ client/trunk/app/desktop/src/com/doogetha/blinkergame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.doogetha.blinkergame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.doogetha.blinkergame.BlinkerGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BlinkerGame(), config);
+	}
+}
