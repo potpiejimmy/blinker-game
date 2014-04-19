5dd2e9e591ac5136c3eca770fcde711732cb444a
liese <liese@d7406fb1-6878-4198-8eeb-e43e52bed576>
2014-04-19 21:11:15 +0000

    added iOS resources folder@

--- client/trunk/app/desktop/src/com/doogetha/blinkergame/desktop/DesktopLauncher.java
+++ client/trunk/app/desktop/src/com/doogetha/blinkergame/desktop/DesktopLauncher.java
@@ -7,7 +7,7 @@ import com.doogetha.blinkergame.BlinkerGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 600;
+		config.width = 360;
 		config.height = 600;
 		new LwjglApplication(new BlinkerGame(), config);
 	}
