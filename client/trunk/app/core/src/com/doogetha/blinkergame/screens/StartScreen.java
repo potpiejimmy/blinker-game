739b94ced48896364f02e50cecd4add63fa509c4
liese <liese@d7406fb1-6878-4198-8eeb-e43e52bed576>
2014-05-03 12:39:02 +0000

    added exit button

--- client/trunk/app/core/src/com/doogetha/blinkergame/screens/StartScreen.java
+++ client/trunk/app/core/src/com/doogetha/blinkergame/screens/StartScreen.java
@@ -9,7 +9,7 @@ import com.doogetha.blinkergame.Utils;
 
 public class StartScreen extends AbstractScreen {
 
-	private Button startButton, startWithBetButton;
+	private Button startButton, startWithBetButton, exitButton;
 	
 	public StartScreen(BlinkerGame game) {
 		super(game);
@@ -20,9 +20,13 @@ public class StartScreen extends AbstractScreen {
 		startWithBetButton = Utils.newTextButton(app, "Start Game (+Bet)", new Runnable() {
 			@Override public void run() { app.setScreen(app.enterBetScreen); }
 		});
+		exitButton = Utils.newTextButton(app, "Exit", new Runnable() {
+			@Override public void run() { Gdx.app.exit(); }
+		});
 
 		startButton.setSize(BlinkerGame.VIEWPORT_SIZE, BlinkerGame.VIEWPORT_SIZE / 4);
 		startWithBetButton.setSize(BlinkerGame.VIEWPORT_SIZE, BlinkerGame.VIEWPORT_SIZE / 4);
+		exitButton.setSize(BlinkerGame.VIEWPORT_SIZE, BlinkerGame.VIEWPORT_SIZE / 4);
 
 		setFixedPositions();
 		
@@ -30,13 +34,15 @@ public class StartScreen extends AbstractScreen {
 		stage.addActor(app.assets.startScreenCar);
 		stage.addActor(startButton);
 		stage.addActor(startWithBetButton);
+		stage.addActor(exitButton);
 	}
 	
 	protected void setFixedPositions() {
-		startButton.setPosition((stage.getViewport().getWorldWidth() - startButton.getWidth())/2, BlinkerGame.BUTTON_SIZE);
-		startWithBetButton.setPosition((stage.getViewport().getWorldWidth() - startWithBetButton.getWidth())/2, BlinkerGame.BUTTON_SIZE/5);
+		startButton.setPosition((stage.getViewport().getWorldWidth() - startButton.getWidth())/2, BlinkerGame.BUTTON_SIZE*9/5);
+		startWithBetButton.setPosition((stage.getViewport().getWorldWidth() - startWithBetButton.getWidth())/2, BlinkerGame.BUTTON_SIZE);
+		exitButton.setPosition((stage.getViewport().getWorldWidth() - exitButton.getWidth())/2, BlinkerGame.BUTTON_SIZE/5);
 		app.assets.startScreenBackground.setPosition(-(app.assets.startScreenBackground.getWidth() - stage.getViewport().getWorldWidth())/2, -(app.assets.startScreenBackground.getHeight() - stage.getViewport().getWorldHeight())/2);
-		app.assets.startScreenCar.setPosition((stage.getViewport().getWorldWidth()-app.assets.startScreenCar.getWidth())/2, (stage.getViewport().getWorldHeight()-app.assets.startScreenCar.getHeight())/2);
+		app.assets.startScreenCar.setPosition((stage.getViewport().getWorldWidth()-app.assets.startScreenCar.getWidth())/2, (stage.getViewport().getWorldHeight()-app.assets.startScreenCar.getHeight())/4*3);
 	}
 	
 	@Override
