ce799532f2769f8e8d59a728c21215ce97c4b5da
liese <liese@d7406fb1-6878-4198-8eeb-e43e52bed576>
2014-05-08 21:34:45 +0000

    added AdMob SDK for iOS

--- client/trunk/app/ios/src/com/doogetha/blinkergame/IOSLauncher.java
+++ client/trunk/app/ios/src/com/doogetha/blinkergame/IOSLauncher.java
@@ -1,19 +1,39 @@
 package com.doogetha.blinkergame;
 
 import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.foundation.NSDictionary;
+import org.robovm.apple.foundation.NSString;
 import org.robovm.apple.uikit.UIApplication;
+import org.robovm.bindings.admob.GADAdSizeManager;
+import org.robovm.bindings.admob.GADBannerView;
 
 import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
 
 public class IOSLauncher extends IOSApplication.Delegate {
+	private IOSApplication app;
+	
     @Override
     protected IOSApplication createApplication() {
         IOSApplicationConfiguration config = new IOSApplicationConfiguration();
 		config.orientationLandscape = true;
 		config.orientationPortrait = true;
-        return new IOSApplication(new BlinkerGame(), config);
+        return app = new IOSApplication(new BlinkerGame(), config);
     }
+
+	@Override
+	public boolean didFinishLaunching (UIApplication application, NSDictionary<NSString, ?> launchOptions) {
+		boolean result = super.didFinishLaunching(application, launchOptions);
+		
+		GADBannerView bannerView = new GADBannerView(GADAdSizeManager.smartBannerPortrait());
+		bannerView.setAdUnitID("ca-app-pub-9069641916384503/7433452077");
+	    bannerView.setRootViewController(app.getUIViewController());
+	    app.getUIWindow().addSubview(bannerView);
+	    
+	    bannerView.loadRequest(null);
+		
+		return result;
+	}
 
     public static void main(String[] argv) {
         NSAutoreleasePool pool = new NSAutoreleasePool();
